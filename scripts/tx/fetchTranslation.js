06f23970e754a18cb13e18dc341f55206a580d4c
Zakum <mirveo@gmail.com>
2024-08-07 14:38:58 -0700

    readd .sig file

--- scripts/tx/fetchTranslation.js
+++ scripts/tx/fetchTranslation.js
@@ -1,10 +1,8 @@
-
+// TODO: Implement
+// DeepL API: Translate ja_JP_unique_strings.txt into en_US_unique_strings.txt
+// See if its possible to upload a fresh glossary every time to account for updates to the glossary.csv
+// Make sure output is the same length as input since we use indices to correlate them
 
 // If the `ja_JP_unique_strings.txt` is too large, the DeepL API will reject it. 
 // One option is to split it into separate server/client files and try again. 
 // An easier but more manual alternative is to paste it into an `.xlsx` spreadsheet, manually upload it to DeepL to translate, and paste the translation back into a `.txt` file then continue the steps.
-
-// Translate ja_JP_strings.txt into en_US_strings.txt
-
-// DeepL API
-// Use glossary
